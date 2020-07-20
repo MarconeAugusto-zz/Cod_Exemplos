a4b1c67b5a7c9103be197a85bf0f2d886eb49f73
Marcone <marcone.p.louzada@gmail.com>
2020-07-19 22:21:23 -0300

    Lendo números do teclado e fazendo a média

---
+++ Python/Comparando_Numeros.py
@@ -0,0 +1,8 @@
+x = int(input("\nDigite o primeiro número: "))
+y = int(input("\nDigite o segundo número: "))
+
+if(x == y):
+	print("Os números são iguais! \n")
+else:
+	med = int((x+y)/2)
+	print("A média dos números é: ", med)
